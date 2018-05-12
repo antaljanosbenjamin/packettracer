767f69c5e4ea86a5d307c81d74449dddcde293dc
Antal János Benjamin <antal.janos.benjamin@gmail.com>
2018-05-13 01:00:56 +0200

    Add some formatting to labels

--- hu.bme.mit.mdsd.packettracer.design/src/hu/bme/mit/mdsd/packettracer/design/Services.java
+++ hu.bme.mit.mdsd.packettracer.design/src/hu/bme/mit/mdsd/packettracer/design/Services.java
@@ -1,12 +1,32 @@
 package hu.bme.mit.mdsd.packettracer.design;
 
+import hu.bme.mit.mdsd.packettracer.Connection;
+
 /**
  * The services class used by VSM.
  */
 public class Services {
     
+	private static String [] UNITS = {"b/s","Kb/s", "Mb/s", "Gb/s", "Tb/s", "Pb/s"};
+	private static double DIVIDER = 1024.0;
     /**
     * See http://help.eclipse.org/neon/index.jsp?topic=%2Forg.eclipse.sirius.doc%2Fdoc%2Findex.html&cp=24 for documentation on how to write service methods.
     */
+	
+	private static String getBandwithWithUnit(double bandwith) {
+		int unitSelector = 1;
+		while(bandwith >= DIVIDER && unitSelector < UNITS.length) {
+			bandwith /= DIVIDER;
+			unitSelector++;
+		}
+		unitSelector--;
+		return String.format("%.2f " + UNITS[unitSelector], bandwith);
+	}
+	
+	public static String getBandwithLabel(Connection connection) {
+		double realBandwith =  Math.min(Math.min(connection.getBandwidth(), connection.getFrom().getBandwidth()),connection.getTo().getBandwidth());
+		return getBandwithWithUnit(realBandwith) + " (" + getBandwithWithUnit(connection.getBandwidth()) + ")";
+		
+	}
    
 }
